8c94f75bd30c5be7631411028da9f85626fbb273
Minh Nguyễn <minhtoetno1@gmail.com>
2019-05-07 14:02:10 +0700

    vkl

--- PtitDrive/app/src/main/java/com/ptit/ptitdrive/MainActivity.java
+++ PtitDrive/app/src/main/java/com/ptit/ptitdrive/MainActivity.java
@@ -1,15 +1,14 @@
 package com.ptit.ptitdrive;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,160 +16,130 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.drive.CreateFileActivityOptions;
 import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveClient;
-import com.google.android.gms.drive.DriveContents;
-import com.google.android.gms.drive.DriveResourceClient;
-import com.google.android.gms.drive.MetadataChangeSet;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "drive-quickstart";
     private static final int REQUEST_CODE_SIGN_IN = 0;
-    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
-    private static final int REQUEST_CODE_CREATOR = 2;
-    private DriveClient mDriveClient;
-    private DriveResourceClient mDriveResourceClient;
-    private Bitmap mBitmapToSave;
-
+    private GoogleSignInOptions signInOptions;
+    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInAccount googleSignInAccount;
+    private Button bt_logout, bt_gg;
+    private TextView tv_name;
+    private SignInButton signInButton;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public static String personName;
+    int login = 1;
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SignInButton signInButton = findViewById(R.id.signed_in_button);
+        signInButton = findViewById(R.id.signed_in_button);
+        bt_logout = findViewById(R.id.bt_logout);
+        bt_gg = findViewById(R.id.bt_gg);
+        tv_name = findViewById(R.id.tv_name);
+
+        sharedPreferences = getSharedPreferences("note", MODE_PRIVATE);
+        login = sharedPreferences.getInt("login", login);
+        name = sharedPreferences.getString("name", "");
+        if(login==1) {
+            bt_gg.setVisibility(View.INVISIBLE);
+            bt_logout.setVisibility(View.INVISIBLE);
+        }else{
+            bt_logout.setVisibility(View.VISIBLE);
+            signInButton.setVisibility(View.INVISIBLE);
+            tv_name.setVisibility(View.VISIBLE);
+            tv_name.setText("Hello "+name);
+        }
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 signIn();
             }
         });
+
+        bt_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestScopes(Drive.SCOPE_FILE).requestScopes(Drive.SCOPE_APPFOLDER).build();
+                googleSignInClient = GoogleSignIn.getClient(MainActivity.this, signInOptions);
+                googleSignInClient.signOut();
+                Toast.makeText(MainActivity.this,"LOGOUT SUCCESSFUlLY", Toast.LENGTH_SHORT).show();
+                bt_logout.setVisibility(View.INVISIBLE);
+                signInButton.setVisibility(View.VISIBLE);
+                bt_gg.setVisibility(View.INVISIBLE);
+                tv_name.setText("");
+                editor = sharedPreferences.edit();
+                editor.remove("login");
+                editor.remove("name");
+                editor.commit();
+            }
+        });
+
+        bt_gg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void signIn() {
         Log.i(TAG, "Start sign in");
-//        Toast.makeText(this, "Click button", Toast.LENGTH_SHORT).show();
         GoogleSignInClient GoogleSignInClient = buildGoogleSignInClient();
         startActivityForResult(GoogleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
     }
 
-    //
     private GoogleSignInClient buildGoogleSignInClient() {
-        GoogleSignInOptions signInOptions =
+        signInOptions =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestScopes(Drive.SCOPE_FILE)
                         .build();
         return GoogleSignIn.getClient(this, signInOptions);
     }
-    private void saveFileToDrive() {
-        // Start by creating a new contents, and setting a callback.
-        Log.i(TAG, "Creating new contents.");
-        final Bitmap image = mBitmapToSave;
 
-        mDriveResourceClient
-                .createContents()
-                .continueWithTask(
-                        new Continuation<DriveContents, Task<Void>>() {
-                            @Override
-                            public Task<Void> then(@NonNull Task<DriveContents> task) throws Exception {
-                                return MainActivity.this.createFileIntentSender(task.getResult(), image);
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Failed to create new contents.", e);
-                            }
-                        });
-    }
-    private Task<Void> createFileIntentSender(DriveContents driveContents, Bitmap image) {
-        Log.i(TAG, "New contents created.");
-        // Get an output stream for the contents.
-        OutputStream outputStream = driveContents.getOutputStream();
-        // Write the bitmap data from it.
-        ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
-        try {
-            outputStream.write(bitmapStream.toByteArray());
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to write file contents.", e);
-        }
-
-        // Create the initial metadata - MIME type and title.
-        // Note that the user will be able to change the title later.
-        MetadataChangeSet metadataChangeSet =
-                new MetadataChangeSet.Builder()
-                        .setMimeType("image/jpeg")
-                        .setTitle("Android Photo.png")
-                        .build();
-        // Set up options to configure and display the create file activity.
-        CreateFileActivityOptions createFileActivityOptions =
-                new CreateFileActivityOptions.Builder()
-                        .setInitialMetadata(metadataChangeSet)
-                        .setInitialDriveContents(driveContents)
-                        .build();
-
-        return mDriveClient
-                .newCreateFileActivityIntentSender(createFileActivityOptions)
-                .continueWith(
-                        new Continuation<IntentSender, Void>() {
-                            @Override
-                            public Void then(@NonNull Task<IntentSender> task) throws Exception {
-                                MainActivity.this.startIntentSenderForResult(task.getResult(), REQUEST_CODE_CREATOR, null, 0, 0, 0);
-                                return null;
-                            }
-                        });
-    }
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case REQUEST_CODE_SIGN_IN:
                 Log.i(TAG, "Sign in request code");
-                // Called after user is signed in.
-                if (resultCode == RESULT_OK) {
+                if (resultCode == RESULT_OK && data!=null) {
                     Log.i(TAG, "Signed in successfully.");
-                    // Use the last signed in account here since it already have a Drive scope.
-                    mDriveClient = Drive.getDriveClient(this, GoogleSignIn.getLastSignedInAccount(this));
-                    // Build a drive resource client.
-                    mDriveResourceClient =
-                            Drive.getDriveResourceClient(this, GoogleSignIn.getLastSignedInAccount(this));
-                    // Start camera.
-//                    startActivityForResult(
-//                            new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
+                    getInfo();
+                    bt_logout.setVisibility(View.VISIBLE);
+                    bt_gg.setVisibility(View.VISIBLE);
+                    signInButton.setVisibility(View.INVISIBLE);
+                    login = 2;
+                    editor = sharedPreferences.edit();
+                    editor.putInt("login", login);
+                    editor.commit();
                 }
-                else
-                    Toast.makeText(this, "signed in not successfull", Toast.LENGTH_SHORT).show();
-                break;
-            case REQUEST_CODE_CAPTURE_IMAGE:
-                Log.i(TAG, "capture image request code");
-                // Called after a photo has been taken.
-                if (resultCode == Activity.RESULT_OK) {
-                    Log.i(TAG, "Image captured successfully.");
-                    // Store the image data as a bitmap for writing later.
-                    mBitmapToSave = (Bitmap) data.getExtras().get("data");
-                    saveFileToDrive();
+                else {
+                    Toast.makeText(MainActivity.this,"Login False",Toast.LENGTH_SHORT).show();
                 }
-                break;
-            case REQUEST_CODE_CREATOR:
-                Log.i(TAG, "creator request code");
-                // Called after a file is saved to Drive.
-                if (resultCode == RESULT_OK) {
-                    Log.i(TAG, "Image successfully saved.");
-                    mBitmapToSave = null;
-                    // Just start the camera again for another photo.
-                    startActivityForResult(
-                            new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
-                }
-                break;
+        }
+    }
+
+    public void getInfo(){
+        googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (googleSignInAccount != null) {
+            personName = googleSignInAccount.getDisplayName();
+            tv_name.setText("Hello "+personName);
+            editor = sharedPreferences.edit();
+            editor.putString("name", personName);
+            editor.commit();
+
+            String personGivenName = googleSignInAccount.getGivenName();
+            String personFamilyName = googleSignInAccount.getFamilyName();
+            String personEmail = googleSignInAccount.getEmail();
+            String personId = googleSignInAccount.getId();
+            Uri personPhoto = googleSignInAccount.getPhotoUrl();
+            Log.i(TAG +" photo", personPhoto+"");
+            Log.i(TAG +" name", personName+"");
         }
     }
 }
